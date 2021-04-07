9b092dbf2c70ee1de506a6247a7f7772207ab7b1
yoichi <itiyou14@gmail.com>
2021-04-07 23:05:32 +0900

    フレーム分割の修正とこれに伴う得点計算式の追記

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -4,14 +4,20 @@
 # 引数を取る
 score = ARGV[0]
 
-# ストライクなら１つの配列に、それ以外なら２つの配列に入れる
 scores = score.split(',')
 frames = []
 shots = []
-scores.each do |s|
+scores.each.with_index do |s, shot_i|
   score = s.to_i
+  if frames.size == 9 # 10フレームの時
+    score = 10 if s == 'X'
+    shots << score
+    frames << shots if shot_i == scores.size - 1 # 最後のショットの時
+    next
+  end
+  # 〜9フレーム目の時
   # ストライクの時
-  if s == 'X'
+  if s == 'X' && frames.size < 9
     frames << [10]
   else # 他の時
     shots << score
@@ -21,15 +27,13 @@ scores.each do |s|
     end
   end
 end
-# 最後にスペアを出した時
-frames << shots unless shots.empty?
 
 total_points_before_addition = 0
 spare_points_add = 0
 strike_points_add = 0
 
-frames.each.with_index do |(first_shot, second_shot), frame_i|
-  total_points_before_addition += first_shot + (second_shot || 0) # 加算前の数値
+frames.each.with_index do |(first_shot, second_shot, third_shot), frame_i|
+  total_points_before_addition += first_shot + (second_shot || 0) + (third_shot || 0) # 加算前の数値
   if !second_shot.nil? && (first_shot + second_shot == 10) && frame_i < 9 # スペアの加算値
     spare_points_add += frames[frame_i + 1][0]
   elsif second_shot.nil? && first_shot == 10 && frame_i < 9 # ストライクの加算値
