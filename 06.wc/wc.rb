f7124402d23b20e2d592aefbc57e9efd046a0ace
yoichi <itiyou14@gmail.com>
2021-04-13 16:42:41 +0900

    標準入力の対応とlコマンドの追加

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,17 +4,19 @@
 require 'optparse'
 
 def main
+  opt = ARGV.getopts('l')
+  opt_l = opt['l']
   file_names = ARGV
-  if file_names
-    for_file_wc(file_names)
+  if file_names != []
+    # lコマンドがあれば引数lはtrue,なければfalse
+    for_file_wc(file_names, opt_l)
   else
     text = $stdin.read
-    print text.count("\n")
+    for_text_wc(text, opt_l)
   end
 end
 
-def for_file_wc(file_names)
-  opt = ARGV.getopts('l')
+def for_file_wc(file_names, opt_l)
   total_line = 0
   total_word = 0
   total_byte = 0
@@ -27,13 +29,24 @@ def for_file_wc(file_names)
     total_line += line_number
     total_word += word_number
     total_byte += byte_number
-    if opt['l']
+    if opt_l
       puts "#{line_number.to_s.rjust(8)} #{file}"
       puts "#{total_line.to_s.rjust(8)} total" if index == file_names.length && index != 1
     else
       puts "#{line_number.to_s.rjust(8)}#{word_number.to_s.rjust(8)}#{byte_number.to_s.rjust(8)} #{file}"
       puts "#{total_line.to_s.rjust(8)}#{total_word.to_s.rjust(8)}#{total_byte.to_s.rjust(8)} total" if index == file_names.length && index != 1
     end
+  end
+end
+
+def for_text_wc(text, opt_l)
+  line_number = text.count("\n")
+  word_number = text.split.count
+  byte_number = text.bytesize
+  if opt_l
+    puts line_number.to_s.rjust(8).to_s
+  else
+    puts "#{line_number.to_s.rjust(8)}#{word_number.to_s.rjust(8)}#{byte_number.to_s.rjust(8)}"
   end
 end
 
