7cf1bdddbe53a5d06ecf6e1a7b15325e1187334b
yoichi <itiyou14@gmail.com>
2021-04-18 08:29:25 +0900

    リーダブルにするための修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -7,39 +7,39 @@ def main
   opt = ARGV.getopts('l')
   opt_l = opt['l']
   file_names = ARGV
-  if file_names != []
+  if file_names.empty?
     # lコマンドがあれば引数lはtrue,なければfalse
-    for_file_wc(file_names, opt_l)
-  else
     text = $stdin.read
-    for_text_wc(text, opt_l)
+    output_for_text_wc(text, opt_l)
+  else
+    output_for_file_wc(file_names, opt_l)
   end
 end
 
-def for_file_wc(file_names, opt_l)
+def output_for_file_wc(file_names, opt_l)
   total_line = 0
   total_word = 0
   total_byte = 0
   file_names.each.with_index(1) do |file, index|
-    text = ''
-    File.open(file) { |f| text = f.read }
+    text = File.read(file)
     line_number = text.count("\n")
     word_number = text.split.count
     byte_number = text.bytesize
     total_line += line_number
     total_word += word_number
     total_byte += byte_number
+    output_total = (index == file_names.length && index != 1)
     if opt_l
       puts "#{line_number.to_s.rjust(8)} #{file}"
-      puts "#{total_line.to_s.rjust(8)} total" if index == file_names.length && index != 1
+      puts "#{total_line.to_s.rjust(8)} total" if output_total
     else
       puts "#{line_number.to_s.rjust(8)}#{word_number.to_s.rjust(8)}#{byte_number.to_s.rjust(8)} #{file}"
-      puts "#{total_line.to_s.rjust(8)}#{total_word.to_s.rjust(8)}#{total_byte.to_s.rjust(8)} total" if index == file_names.length && index != 1
+      puts "#{total_line.to_s.rjust(8)}#{total_word.to_s.rjust(8)}#{total_byte.to_s.rjust(8)} total" if output_total
     end
   end
 end
 
-def for_text_wc(text, opt_l)
+def output_for_text_wc(text, opt_l)
   line_number = text.count("\n")
   word_number = text.split.count
   byte_number = text.bytesize
