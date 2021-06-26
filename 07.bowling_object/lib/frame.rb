0435c44e7c3daeefae7428c7374e94adcd2ab38e
yoichi <itiyou14@gmail.com>
2021-06-27 08:34:21 +0900

    frameクラスの冗長性を修正

--- 07.bowling_object/lib/frame.rb
+++ 07.bowling_object/lib/frame.rb
@@ -1,29 +1,22 @@
 # frozen_string_literal: true
 
 class Frame
+  attr_reader :first_shot, :second_shot
   def initialize(first_mark, second_mark = nil, third_mark = nil)
-    @first_shot = Shot.new(first_mark)
-    @second_shot = Shot.new(second_mark)
-    @third_shot = Shot.new(third_mark)
+    @first_shot = Shot.new(first_mark).score
+    @second_shot = Shot.new(second_mark)&.score
+    @third_shot = Shot.new(third_mark)&.score
   end
 
   def score
-    @first_shot.score + @second_shot&.score.to_i + @third_shot&.score.to_i
-  end
-
-  def first_shot
-    @first_shot.score
-  end
-
-  def second_shot
-    @second_shot&.score
+    @first_shot + @second_shot.to_i + @third_shot.to_i
   end
 
   def strike?
-    @first_shot.score == 10
+    @first_shot == 10
   end
 
   def spare?
-    (@first_shot.score + @second_shot&.score.to_i) == 10 && @first_shot.score != 10
+    (@first_shot + @second_shot.to_i) == 10 && @first_shot != 10
   end
 end
